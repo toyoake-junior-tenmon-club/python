c43673d72eac56bdbc752aa3b419e02195a633f7
toyoake-junior-tenmon-club <105533273+toyoake-junior-tenmon-club@users.noreply.github.com>
2024-05-05 20:48:19 +0900

    Add files via upload

--- stella_binary.py
+++ stella_binary.py
@@ -3,30 +3,35 @@ import numpy as np
 import sys
 import cv2
 
-levels = [150, 170]
-
+#
+# 多段ノイズ除去 
+#
 def main(img_in, img_out):
-    im = cv2.imread(img_in, cv2.IMREAD_GRAYSCALE)
+    # 画像をグレースケールで読み取り
+    im_gray = cv2.imread(img_in, cv2.IMREAD_GRAYSCALE)
 
     # マスクパターン作成
-    (height, width) = im.shape
-    mask = np.zeros((height, width,3), np.uint8)
-    mask_0 = cv2.rectangle(mask, (1,1), (width,int(2*height/3)-110), (255,255,255), -1)
-    mask_0 = cv2.cvtColor(mask_0, cv2.COLOR_BGR2GRAY)
+    (height, width) = im_gray.shape
 
-    mask_1 = cv2.rectangle(mask, (1,int(2*height/3)-115), (width,height), (255,255,255), -1)
-    mask_1 = cv2.cvtColor(mask_1, cv2.COLOR_BGR2GRAY)
+    # 輝度レベル
+    levels = [100, 155, 170]
+    p_starts = [(1,1), (1,int(height/3)-51), (1, int(2*height/3))]
+    p_ends = [(width,int(height/3)-50), (width, int(2*height/3)-1), (width,height)]
 
-    im_mask0 = cv2.bitwise_and(im, mask_0)
-    _, im_filter0 = cv2.threshold(im_mask0, levels[0], 255, cv2.THRESH_BINARY)
+    im_out = cv2.cvtColor(np.zeros((height, width,3), np.uint8), cv2.COLOR_BGR2GRAY)
+    for n in range(0,len(levels)):
+        mask = np.zeros((height, width,3), np.uint8)
+        im_mask = cv2.rectangle(mask, p_starts[n], p_ends[n], (255,255,255), -1)
+        im_mask = cv2.cvtColor(im_mask, cv2.COLOR_BGR2GRAY)
+        im_area = cv2.bitwise_and(im_gray, im_mask)
+        
+        _, im_filter = cv2.threshold(im_area, levels[n], 255, cv2.THRESH_BINARY)
+        cv2.imwrite('debug' + str(n) + '.jpg', im_filter)
 
-    im_mask1 = cv2.bitwise_and(im, mask_1)
-    _, im_filter1 = cv2.threshold(im_mask1, levels[1], 255, cv2.THRESH_BINARY)
+        # 画像合成
+        im_out = cv2.bitwise_or(im_out, im_filter)
 
-    im_out = im_filter0
-    im_out[mask_0 == 0] = im_filter1[mask_0 == 0]
-
-    cv2.imwrite(img_out, im_filter0)
+    cv2.imwrite(img_out, im_out)
 
 
 if __name__ == '__main__':
